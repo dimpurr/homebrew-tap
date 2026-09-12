3a228e1300bc6fedf28278c3d45547e4fdce6898
dimpurr <dimpurr@live.com>
2026-09-12 19:40:16 +0100

    cask: add Voltscope 0.8.1

---
+++ Casks/voltscope.rb
@@ -0,0 +1,19 @@
+cask "voltscope" do
+  version "0.8.1"
+  sha256 "735859650eab6b0de72283868a02f893b0c8de92d7494bcb4d2860e2fd731c16"
+
+  url "https://github.com/dimpurr/voltscope/releases/download/v#{version}/Voltscope-#{version}-universal2.dmg",
+      verified: "github.com/dimpurr/voltscope/"
+  name "Voltscope"
+  desc "Native macOS battery and energy-history monitor"
+  homepage "https://voltscope.dimp.studio/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: :ventura
+
+  app "Voltscope.app"
+end
