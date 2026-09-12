3911797a85a7edfb4be19285706622a8f936d4aa
dimpurr <dimpurr@live.com>
2026-09-12 19:46:12 +0100

    cask: use standard GitHub release URL

--- Casks/voltscope.rb
+++ Casks/voltscope.rb
@@ -2,8 +2,7 @@ cask "voltscope" do
   version "0.8.1"
   sha256 "735859650eab6b0de72283868a02f893b0c8de92d7494bcb4d2860e2fd731c16"
 
-  url "https://github.com/dimpurr/voltscope/releases/download/v#{version}/Voltscope-#{version}-universal2.dmg",
-      verified: "github.com/dimpurr/voltscope/"
+  url "https://github.com/dimpurr/voltscope/releases/download/v#{version}/Voltscope-#{version}-universal2.dmg"
   name "Voltscope"
   desc "Battery and energy-history monitor"
   homepage "https://voltscope.dimp.studio/"
